f1555f5f7b1495df358ae4a223ec0bd7a151e4a3
GiviTskitishvili <tgivi@yahoo.com>
2024-01-24 01:52:25 +0400

    corrected

--- ex8.js
+++ ex8.js
@@ -1,8 +1,8 @@
 const names = ["John Doe", "Alice Smith", "Bob Johnson"];
 
 const initials = names.map((name) => {
-  const nameParts = name.split(" "); // Split the name into parts
-  return nameParts[0][0] + nameParts[1][0]; // Take the first character of each part
+  const nameParts = name.split(" ");
+  return nameParts[0][0] + nameParts[1][0];
 });
 
 console.log(initials);
